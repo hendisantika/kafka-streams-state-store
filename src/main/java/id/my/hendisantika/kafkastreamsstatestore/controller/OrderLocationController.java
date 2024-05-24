5dfad33befe6b5994043a00e60c95ff98d5d38af
hendisantika <hendisantika@yahoo.co.id>
2024-05-24 09:23:48 +0700

    Add getOrderLocation in OrderLocationController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkastreamsstatestore/controller/OrderLocationController.java
+++ src/main/java/id/my/hendisantika/kafkastreamsstatestore/controller/OrderLocationController.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.kafkastreamsstatestore.controller;
 
+import id.my.hendisantika.kafkastreamsstatestore.dto.OrderLocation;
 import id.my.hendisantika.kafkastreamsstatestore.service.OrderLocationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +27,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderLocationController {
 
     private final OrderLocationService orderLocationService;
+
+    /**
+     * {@code GET  /the order location} : order location for of an order.
+     *
+     * @param orderNumber order number
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the order location in body.
+     */
+    @GetMapping("/order-location/{orderNumber}")
+    public ResponseEntity<OrderLocation> getOrderLocation(@PathVariable String orderNumber) {
+        log.info("REST request to get location of Order");
+        OrderLocation orderLocation = orderLocationService.getOrderLocation(orderNumber);
+        return ResponseEntity.ok().body(orderLocation);
+    }
 }
