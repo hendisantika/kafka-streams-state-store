d682f36021ed01390731bdf8a469100abe4e6210
hendisantika <hendisantika@yahoo.co.id>
2024-05-24 09:20:45 +0700

    Add OrderLocationStreamsProcessor🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkastreamsstatestore/kafka/OrderLocationStreamsProcessor.java
+++ src/main/java/id/my/hendisantika/kafkastreamsstatestore/kafka/OrderLocationStreamsProcessor.java
@@ -1,11 +1,10 @@
 package id.my.hendisantika.kafkastreamsstatestore.kafka;
 
 import id.my.hendisantika.kafkastreamsstatestore.dto.OrderLocation;
-import org.apache.kafka.streams.processor.api.Processor;
-import org.apache.kafka.streams.processor.api.ProcessorContext;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.KeyValueStore;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -21,10 +20,9 @@ import java.util.Objects;
  * Time: 09:17
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Component
 public class OrderLocationStreamsProcessor implements Processor<String, OrderLocation> {
-
-    private final Logger log = LoggerFactory.getLogger(OrderLocationStreamsProcessor.class);
 
     private KeyValueStore<String, OrderLocation> stateStore;
 
@@ -35,5 +33,19 @@ public class OrderLocationStreamsProcessor implements Processor<String, OrderLoc
     public void init(ProcessorContext context) {
         stateStore = context.getStateStore(stateStoreName);
         Objects.requireNonNull(stateStore, "State store can't be null");
+    }
+
+    @Override
+    public void process(String orderNumber, OrderLocation orderLocation) {
+        log.info("Streams Request to save process Order Location : {}", orderLocation);
+
+        //add processing the kafka message here like save to NoSQL DB, Elastic Search, do aggregation
+
+        stateStore.put(orderNumber, orderLocation);
+    }
+
+    @Override
+    public void close() {
+
     }
 }
